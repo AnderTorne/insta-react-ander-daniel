bb99f5bcf999037ab0de64c77eb2e7555f731b6f
Ander Torne <aatorne@outlook.com>
2022-04-28 22:02:21 -0500

    basic front

--- insta-react-ander-daniel/src/App.js
+++ insta-react-ander-daniel/src/App.js
@@ -27,7 +27,7 @@ function App() {
           />
         </div>
         
-        <h1>Instagram Clone</h1>
+        {/* <h1>Instagram Clone</h1> */}
         {
           posts.map(post =>(
             <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
